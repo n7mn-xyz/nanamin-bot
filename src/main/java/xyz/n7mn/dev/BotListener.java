561b09f939ab69f79b0013bd75262e2b1f060ce9
kanasaki15 <kassi_0123@kassi-hp.info>
2021-04-02 20:26:47 +0900

    1から書き直したい

---
+++ src/main/java/xyz/n7mn/dev/BotListener.java
@@ -0,0 +1,6 @@
+package xyz.n7mn.dev;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BotListener extends ListenerAdapter {
+}
--- src/main/java/xyz/n7mn/dev/NanamiMain.java
+++ src/main/java/xyz/n7mn/dev/NanamiMain.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
-import xyz.n7mn.dev.Command.money.MoneySystem;
 
 public class NanamiMain {
 
@@ -14,16 +13,14 @@ public class NanamiMain {
 
         try {
 
-            String json = NanamiFunction.fileRead("./token.json");
-
-            String token = new Gson().fromJson(json, String.class);
+            String token = "";
 
             JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_VOICE_STATES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_EMOJIS)
-                    .addEventListeners(new EventListener())
+                    .addEventListeners(new BotListener())
                     .enableCache(CacheFlag.VOICE_STATE)
                     .enableCache(CacheFlag.EMOTE)
                     .setMemberCachePolicy(MemberCachePolicy.ALL)
-                    .setActivity(Activity.playing("ななみちゃんbot Ver " + NanamiFunction.getVersion()))
+                    .setActivity(Activity.playing("ななみちゃんbot Ver 2.0-dev"))
                     .build();
 
 
